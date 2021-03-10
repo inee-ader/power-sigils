dfff1b37b709b45851dd3d59aa4098f081828206
xoliio <xoliio@gmail.com>
2021-03-10 12:32:15 -0800

    more responsiveness with css

--- src/Letter.js
+++ src/Letter.js
@@ -4,7 +4,7 @@ import './styles/Letter.css'
 const Letter = (props) => {
     return (
         <div className='letter' onClick={props.click}>
-            <h1 className='letter__h'>{props.letter}</h1>
+            {props.letter}
         </div>
     );
 }
